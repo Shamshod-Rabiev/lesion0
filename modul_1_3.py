5afad661e29d3753a5d821ccae6ce5ca198f9d10
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-17 21:03:32 +0300

    Add files via upload

--- modul_1_3.py
+++ modul_1_3.py
@@ -2,7 +2,7 @@ name = "Mustafa"
 print(name, type(name))
 age = 24
 print(age, type(age))
-new_age = 30
-print(new_age, type(new_age))
-is_student = "True"
-print(is_student, type(is_student))
+age = (age+1)
+print(age, type(age+1))
+is_student = True
+print(is_student, type(True))
