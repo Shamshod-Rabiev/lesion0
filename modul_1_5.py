390bcfdd7dfb19b2f56ae30c6a1d063202742a20
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-18 22:15:20 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 5, 8, 3 )
+print(immutable_var)
+#immutable_var [0] = 50 #кортеж – это неизменяемая упорядоченная коллекция, которая может содержать в себе разные типы данных.
+mutable_list = ([2, 4, 6], 8)
+print(mutable_list)
+mutable_list [0][0] = 5
+print(mutable_list)
