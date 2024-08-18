1c4b5d85e3f19bdaa46968cfa5520dd66a8a1a9b
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-18 12:53:09 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,10 @@
+from pydoc import replace
+
+name = "my_string"
+print(input(name))
+print(len(name))
+print("my_string" .upper())
+print("my_string" .upper().lower())
+print("my_string" .replace("_", ""))
+print("my_string"[0])
+print("my_string" [-1])
