2999b14aea293a0a85c15b3dc07aab3251e9fcd1
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-19 11:16:21 +0300

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {"Mustafa" : 2000, "Fatima" : 2022, "Dima" : 9267773399,}
+print(my_dict)
+print(my_dict.get("Mustafa"))
+print(my_dict.get("Mama"))
+my_dict.update({"Brat" : 9099993378 , "Lena" : 9225784430})
+del my_dict["Dima"]
+print(my_dict)
+my_set = {55, 10, 44, 22, 10, 22, 44, 22, 23}
+print(my_set)
+my_set.add(200)
+my_set.add(999)
+my_set.discard(22)
+print(my_set)
