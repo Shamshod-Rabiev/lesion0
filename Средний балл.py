39b93c226f291ebd1fa08e6796e20582b29e42c7
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-21 17:43:39 +0300

    Add files via upload

---
+++ Средний балл.py
@@ -0,0 +1,26 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students ={'Johnny', 'Bilbo', 'Steve', 'khendrik', 'Aaron'}
+sort_students = sorted(students)
+dict_some = {}
+avg1 = sum(grades[0]) / len(grades[0])
+avg2 = sum(grades[1]) / len(grades[1])
+avg3 = sum(grades[2]) / len(grades[2])
+avg4 = sum(grades[3]) / len(grades[3])
+avg5 = sum(grades[4]) / len(grades[4])
+
+dict_some[sort_students[0]] = avg1
+dict_some[sort_students[1]] = avg2
+dict_some[sort_students[2]] = avg3
+dict_some[sort_students[4]] = avg4
+dict_some[sort_students[3]] = avg5
+print(dict_some)
+
+
+#print(grades)
+
+#dict_some = {}
+#avg1 = sum(grades[20] / len(grades[1]))
+#print(sum())
+
+#grades = tuple(grades)
+#print(grades)
