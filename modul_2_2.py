9471ab868121691469e2d4ed5c2d8ddd96c699da
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-23 17:22:25 +0300

    Add files via upload

---
+++ modul_2_2.py
@@ -0,0 +1,31 @@
+first = int(input("первый"))
+second = int(input("второй"))
+third = int(input("третий"))
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+
+
+
+
+
+
+
+
+
+#if first % 123 == 0:
+   # print("Fizz")
+#elif first % 456 == 0:
+    #print('Buzz')
+#elif first % 789 == 0:
+    #print('FizzBuzz')
+
+#elif first % 42 == 0:
+    #print("Fizz")
+#elif first % 69 == 0:
+    #print('Buzz')
+#elif first % 42 == 0:
+    #print('FizzBuzz')
