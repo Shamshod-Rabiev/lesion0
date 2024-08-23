8f2c1512d164bbac6a1205d70bd32c21d2391da1
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-23 17:23:43 +0300

    Add files via upload

--- modul_2_2.py
+++ modul_2_2.py
@@ -8,24 +8,3 @@ elif first == second or second == third or first == third:
 else:
     print(0)
 
-
-
-
-
-
-
-
-
-#if first % 123 == 0:
-   # print("Fizz")
-#elif first % 456 == 0:
-    #print('Buzz')
-#elif first % 789 == 0:
-    #print('FizzBuzz')
-
-#elif first % 42 == 0:
-    #print("Fizz")
-#elif first % 69 == 0:
-    #print('Buzz')
-#elif first % 42 == 0:
-    #print('FizzBuzz')
