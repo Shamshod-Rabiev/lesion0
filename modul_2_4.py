069452447f02003de7c5560afe557c6f856d65cc
Shamshod-Rabiev <Rabievsamsod9@gmail.com>
2024-08-29 10:35:06 +0300

    Add files via upload

--- modul_2_4.py
+++ modul_2_4.py
@@ -2,11 +2,11 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 a = 0
-for number in range(2, len(numbers) + 1):
+for number in range(2, len(numbers) + 2):
     is_prime = True
     for j in range(2, number):
         if number % j == 0:
-            a = a + 1
+            a = a + 2
     if a == 0:
         primes.append(number)
     else:
